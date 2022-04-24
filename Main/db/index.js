80fff9522b915e391d017d9c7cec80ee8757fbb2
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-04-24 19:25:30 -0400

    updating readme

---
+++ Main/db/index.js
@@ -0,0 +1,1 @@
+// You might want to use a separate file that contains functions for performing specific SQL queries you will need to use. A constructor function or class could be helpful for organizing these. You might also want to include a `seeds.sql` file to pre-populate your database, making the development of individual features much easier.
